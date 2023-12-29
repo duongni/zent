ad6fd2534612a536d16adfac501d28073d7d9703
duongni <133175915+duongni@users.noreply.github.com>
2023-12-29 12:47:18 -0500

    work on the hotel page. Add first card to the page.

--- app/hotels/page.tsx
+++ app/hotels/page.tsx
@@ -1,10 +1,36 @@
-import React from "react";
-export const metadata = {
-  title: "Hotels",
-  description: "Curated List of Hotels.",
-};
-function Page() {
-  return <h3 className="mt-[200px]">list of hotels</h3>;
-}
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import { SlHeart } from "react-icons/sl";
 
+function Page() {
+  const [isLiked, setLike] = useState(false);
+  const handleheartClick = () => {
+    setLike(!isLiked);
+  };
+  return (
+    <div className="mt-[200px]  mx-3 relative">
+      <Image
+        className="hover:opacity-75 rounded"
+        src="/images/arje/arje_dining-1.jpeg"
+        alt="Arje Property"
+        width={400}
+        height={600}
+        priority={false} // {false} | {true}
+      />
+      <SlHeart
+        size={20}
+        className={`absolute top-2 left-2 cursor-pointer ${
+          isLiked ? "text-yellow-100" : "text-transparent "
+        }`}
+        onClick={handleheartClick}
+      />
+
+      <h4 className="font-normal text-2xl tracking-widest my-2.5">
+        Hotel Name 1
+      </h4>
+      <p> Location | 1 BD 2 BA | Price </p>
+    </div>
+  );
+}
 export default Page;
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -23,11 +23,13 @@ function pages() {
       >
         <div className="grid grid-cols-2 w-3/4  my-2 gap-2">
           <input
+            id="firstName"
             type="text"
             placeholder="First name"
             className="border-b-2 focus:outline-none my-11 text-sm"
           />
           <input
+            id="lastName"
             type="text"
             placeholder="Last name"
             className="border-b-2 focus:outline-none my-11 text-sm"
@@ -35,17 +37,20 @@ function pages() {
         </div>
         <div className="grid grid-cols-1 w-3/4  my-2 gap-14 ">
           <input
+            id="email"
             type="text"
             placeholder="Email Address"
             className="border-b-2 text-sm focus:outline-none"
           />
           <input
+            id="userName"
             type="text"
             placeholder="Username"
             className="border-b-2 text-sm focus:outline-none"
           />
           <div className="flex flex-row justify-between">
             <input
+              id="passWord"
               type="password"
               placeholder="Password"
               className="border-b-2 text-sm focus:outline-none w-11/12"
@@ -57,7 +62,7 @@ function pages() {
         </div>
         <button
           type="submit"
-          className=" my-20 font-bold tracking-widest hover:text-gray-400 hover:border-solid hover:border-black"
+          className=" my-20 font-bold tracking-widest hover:text-gray-400 "
         >
           <Link href="../journal/page">Register</Link>
         </button>
